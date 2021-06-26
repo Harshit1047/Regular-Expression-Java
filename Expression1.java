6399ecd7db03f8ba1e9f69e2501bee1f80e4e4db
Harshit Pratap Singh <86252946+Harshit1047@users.noreply.github.com>
2021-06-26 23:23:35 +0530

    Add files via upload

---
+++ Expression1.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+class expr1{
+    public static void main(String[] args)
+    {
+        String str="a";
+        boolean str1=str.matches("[abc]");
+        System.out.println(str1);
+
+    }
+}
