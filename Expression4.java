c2702ea96b5c35e8e92a5d38772c5641265fdd65
Harshit Pratap Singh <86252946+Harshit1047@users.noreply.github.com>
2021-06-26 23:53:26 +0530

    Add files via upload

--- Expression4.java
+++ Expression4.java
@@ -2,8 +2,8 @@ import java.lang.*;
 class expr4{
     public static void main(String[] args)
     {
-        String str="b8";
-        boolean str1=str.matches("[a-z][0-9]");
+        String str="b";
+        boolean str1=str.matches("a|b");
         System.out.println(str1);
 
     }
