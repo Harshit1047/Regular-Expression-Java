1f827f1722e4e20dec61c0f8eb21aa8b78333f3f
Harshit Pratap Singh <86252946+Harshit1047@users.noreply.github.com>
2021-06-26 23:34:42 +0530

    Add files via upload

---
+++ Expression4.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+class expr4{
+    public static void main(String[] args)
+    {
+        String str="b8";
+        boolean str1=str.matches("[a-z][0-9]");
+        System.out.println(str1);
+
+    }
+}
