ab778a96d1e99842991555549f4d7ba3754e712d
Harshit Pratap Singh <86252946+Harshit1047@users.noreply.github.com>
2021-06-27 00:03:09 +0530

    Add files via upload

---
+++ Expression5.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+class expr5{
+    public static void main(String[] args)
+    {
+        String str="abc";
+        boolean str1=str.matches("abc");
+        System.out.println(str1);
+
+    }
+}
