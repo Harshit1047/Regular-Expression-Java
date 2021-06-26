194faa8ad4e8fd36f88b7005093502a02ec9e5ad
Harshit Pratap Singh <86252946+Harshit1047@users.noreply.github.com>
2021-06-26 23:29:05 +0530

    Add files via upload

---
+++ Expression3.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+class expr3{
+    public static void main(String[] args)
+    {
+        String str="n";
+        boolean str1=str.matches("[a-zA-Z0-9]");
+        System.out.println(str1);
+
+    }
+}
