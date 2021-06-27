a1255a5010ee083e8221a0f9a9e08a494457528d
Harshit Pratap Singh <86252946+Harshit1047@users.noreply.github.com>
2021-06-27 23:38:15 +0530

    Add files via upload

---
+++ MetaCharacter1.java
@@ -0,0 +1,9 @@
+import java.lang.*;
+class Meta1{
+    public static void main(String[] args)
+    {
+        String str="j";
+        boolean str1=str.matches("\\D");
+        System.out.println(str1);
+    }
+}
