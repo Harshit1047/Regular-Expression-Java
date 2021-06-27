122b31fdcc32d5fb0ca52299ed4749e806685bb2
Harshit Pratap Singh <86252946+Harshit1047@users.noreply.github.com>
2021-06-27 23:41:14 +0530

    Add files via upload

---
+++ MetaCharacter2.java
@@ -0,0 +1,9 @@
+import java.lang.*;
+class Meta2{
+    public static void main(String[] args)
+    {
+        String str=" ";
+        boolean str1=str.matches("\\s");
+        System.out.println(str1);
+    }
+}
