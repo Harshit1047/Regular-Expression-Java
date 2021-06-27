e29f9dab9dac6ad454973618652f69f568601651
Harshit Pratap Singh <86252946+Harshit1047@users.noreply.github.com>
2021-06-27 23:42:53 +0530

    Add files via upload

---
+++ MetaCharacter4.java
@@ -0,0 +1,9 @@
+import java.lang.*;
+class Meta4{
+    public static void main(String[] args)
+    {
+        String str="D";
+        boolean str1=str.matches("\\w");
+        System.out.println(str1);
+    }
+}
---
+++ MetaCharacter5.java
@@ -0,0 +1,9 @@
+import java.lang.*;
+class Meta5{
+    public static void main(String[] args)
+    {
+        String str="$";
+        boolean str1=str.matches("\\W");
+        System.out.println(str1);
+    }
+}
