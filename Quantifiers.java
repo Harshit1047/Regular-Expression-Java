9beb4f8891bf9513cf31e02d1d016df8780e6d2c
Harshit Pratap Singh <86252946+Harshit1047@users.noreply.github.com>
2021-06-27 23:52:35 +0530

    Add files via upload

---
+++ Quantifiers.java
@@ -0,0 +1,9 @@
+import java.lang.*;
+class Quant{
+    public static void main(String[] args)
+    {
+        String str="abcdef";
+        boolean str1=str.matches(".*");
+        System.out.println(str1);
+    }
+}
